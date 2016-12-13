45371e30deedae102b4d1c2147e5de61f478aa3f
Paulo Costa <ei12099@fe.up.pt>
2016-12-13 18:05:55 +0100

    scores routes added

--- Server/models/db.js
+++ Server/models/db.js
@@ -12,7 +12,7 @@ client.connect(function (err) {
 
 exports.findUser = function (user, pass, callback) {
     // execute a query on our database
-    client.query('SELECT username, password FROM users where username = $1::text', [user], function (err, result) {
+    client.query('SELECT username, password FROM users where username = $1', [user], function (err, result) {
         if (err)
             return callback(false);
 
@@ -21,7 +21,7 @@ exports.findUser = function (user, pass, callback) {
 }
 
 exports.insertUser = function (user, pass, callback) {
-    client.query('insert into users (high_score,password,username,exp_points) values (0, $1,$2,0)', [user, pass], function (err, result) {
+    client.query('insert into users (high_score,password,username,exp_points) values (0, $1,$2,0)', [user, pass], function (err) {
         if (err)
             return callback(false);
 
@@ -29,6 +29,41 @@ exports.insertUser = function (user, pass, callback) {
     });
 }
 
+exports.getHighScore = function (user, callback) {
+    client.query('SELECT high_score FROM users where username = $1', [user], function (err, result) {
+        if (err)
+            return callback(false);
+
+        return callback(result.rows[0]);
+    });
+}
+
+exports.updateHighScore = function (user, high_score, callback) {
+    client.query('UPDATE users SET high_score = $1 WHERE username=$2', [high_score, user], function (err) {
+        if (err)
+            return callback(false);
+
+        return callback(true);
+    });
+}
+
+exports.getExpPoints = function (user, callback) {
+    client.query('SELECT exp_points FROM users where username = $1', [user], function (err, result) {
+        if (err)
+            return callback(false);
+
+        return callback(result.rows[0]);
+    });
+}
+
+exports.updateExpPoints = function (user, exp_points, callback) {
+    client.query('UPDATE users SET exp_points = $1 WHERE username=$2', [exp_points, user], function (err) {
+        if (err)
+            return callback(false);
+
+        return callback(true);
+    });
+}
 
 // disconnect the client
 /*client.end(function (err) {
--- Server/routes/index.js
+++ Server/routes/index.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 var auth = require('../controllers/auth');
+var scores = require('../controllers/scores');
 
 //AUTH
 router.route('/login').post(auth.loginUser);
 router.route('/register').post(auth.registerUser);
+
+//SCORES
+router.route('/getScore').get(scores.getHighScore);
+router.route('/getPoints').get(scores.getExpPoints);
+router.route('/updateScore').post(scores.updateHighScore);
+router.route('/updatePoints').post(scores.updateExpPoints);
 
 module.exports = router;
