632cb45b3b501e9c2ab9a184810637f4cca3d27a
Paulo Costa <ei12099@fe.up.pt>
2016-12-22 21:15:31 +0000

    ajax working

---
+++ Tetris/platforms/browser/www/js/auth.js
@@ -0,0 +1,7 @@
+/**
+ * Created by paulo on 22/12/2016.
+ */
+
+$("#auth-button").click(function () {
+    alert("merda crl");
+});
---
+++ Tetris/www/js/auth.js
@@ -0,0 +1,7 @@
+/**
+ * Created by paulo on 22/12/2016.
+ */
+
+$("#auth-button").click(function () {
+    /*ajax call aqui*/
+});
