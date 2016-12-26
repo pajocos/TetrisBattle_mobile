711e1c14c2b1e9384a5572e70a4afe051cb7b2f4
Paulo Costa <ei12099@fe.up.pt>
2016-12-26 16:02:25 +0000

    new form. login register working well

--- Tetris/platforms/android/assets/www/js/auth.js
+++ Tetris/platforms/android/assets/www/js/auth.js
@@ -2,10 +2,29 @@
  * Created by paulo on 22/12/2016.
  */
 
-$("#auth-button").click(function (e) {
+$(function() {
+    $('#login-form-link').click(function(e) {
+        $("#login-form").delay(100).fadeIn(100);
+        $("#register-form").fadeOut(100);
+        $('#register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    $('#register-form-link').click(function(e) {
+        $("#register-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
 
-    var username = $('#username').val();
-    var password = $('#password').val();
+});
+
+$("#login-submit").click(function (e) {
+
+    var username = $('#username1').val();
+    var password = $('#password1').val();
+    var rememberMe = $('#checkbox1').val();
 
     $.ajax({
         type: "POST",
@@ -13,14 +32,41 @@ $("#auth-button").click(function (e) {
         url: "http://192.168.1.5:3000/API/login",
         data: $.param({username: username, password: password}),
         dataType: "text",
-        success: function (data) {
-            console.log(data);
-            alert("Success");
+        success: function () {
+            window.location = "home.html";
         },
         error: function (result) {
             console.log(result);
-            alert("Error");
+            alert("Check your username and password");
         }
     });
     e.preventDefault();
+});
+
+$("#register-submit").click(function (e) {
+
+    var username = $('#username2').val();
+    var password = $('#password2').val();
+    var confirmPassword = $('#confirm-password').val();
+
+    if (confirmPassword !== password) {
+        alert("Passwords doesn't match!");
+        e.preventDefault();
+    }
+    else {
+        $.ajax({
+            type: "POST",
+            contentType: "application/x-www-form-urlencoded",
+            url: "http://192.168.1.5:3000/API/register",
+            data: $.param({username: username, password: password}),
+            dataType: "text",
+            success: function () {
+                alert("Success! Login and enjoy the game")
+            },
+            error: function (result) {
+                alert("Some error happened. Try again please");
+                e.preventDefault();
+            }
+        });
+    }
 });
--- Tetris/platforms/browser/www/js/auth.js
+++ Tetris/platforms/browser/www/js/auth.js
@@ -2,10 +2,30 @@
  * Created by paulo on 22/12/2016.
  */
 
-$("#auth-button").click(function (e) {
+$(function() {
+    $('#login-form-link').click(function(e) {
+        $("#login-form").delay(100).fadeIn(100);
+        $("#register-form").fadeOut(100);
+        $('#register-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
+    $('#register-form-link').click(function(e) {
+        $("#register-form").delay(100).fadeIn(100);
+        $("#login-form").fadeOut(100);
+        $('#login-form-link').removeClass('active');
+        $(this).addClass('active');
+        e.preventDefault();
+    });
 
-    var username = $('#username').val();
-    var password = $('#password').val();
+});
+
+$("#login-submit").click(function (e) {
+
+    var username = $('#username1').val();
+    var password = $('#password2').val();
+    var rememberMe = $('#checkbox1').val();
+    console.log(rememberMe);
 
     $.ajax({
         type: "POST",
@@ -13,13 +33,12 @@ $("#auth-button").click(function (e) {
         url: "http://192.168.1.5:3000/API/login",
         data: $.param({username: username, password: password}),
         dataType: "text",
-        success: function (data) {
-            console.log(data);
-            alert("Success");
+        success: function () {
+            window.location = "home.html";
         },
         error: function (result) {
             console.log(result);
-            alert("Error");
+            alert("Check your username and password");
         }
     });
     e.preventDefault();
